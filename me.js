86900d28ebde2a112b990fdbc2fff445d91c46b0
Emilio Rodriguez <emilioerodriguez@lewisu.edu>
2026-03-02 01:58:33 -0600

    added js stuff

---
+++ me.js
@@ -0,0 +1,19 @@
+const funFacts = [
+    "I have met former President Biden.",
+    "I served 5 years in the United States Marine Corps.",
+    "I enjoy reptile care.",
+    "I like working out regularly.",
+    "I am studying Cybersecurity."
+];
+
+let currentFactIndex = 0;
+
+document.getElementById("funFactBtn").addEventListener("click", function() {
+    currentFactIndex = (currentFactIndex + 1) % funFacts.length;
+    document.getElementById("funFactText").textContent = funFacts[currentFactIndex];
+});
+
+
+document.getElementById("myName").addEventListener("click", function() {
+    this.style.color = "#00bfff"; // change to your favorite color
+});
